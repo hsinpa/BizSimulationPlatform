410233846bb3c0cc679ce089478f88d91c89bf68
hsinpa <hsinpa@gmail.com>
2020-06-22 17:44:43 +0800

    Initial Update

---
+++ src/routing/index.ts
@@ -0,0 +1,25 @@
+import {XLSXParser} from '../xlsx/XLSXParser';
+const path = require('path')
+
+module.exports =  (router:any, rootPath:string) => {
+  router.get('/', async function (ctx:any, next:any) {
+    ctx.state = {
+      title: 'HSINPA'
+    };
+
+    await ctx.render('index', {title: "HSINPA"});
+  });
+
+  router.get('/excel', async function (ctx:any, next:any) {
+    let excelFilePath : string = path.join(rootPath, 'public', 'assets','excel', 'AdvantexeSimExample.xlsx');
+    console.log(excelFilePath);
+
+    let xlsx = new XLSXParser(excelFilePath);
+
+    xlsx.Execute();
+
+    ctx.body = rootPath;
+
+    return ctx;
+  });
+}
---
+++ src/xlsx/XLSXParser.ts
@@ -0,0 +1,15 @@
+import * as xlsx from 'xlsx';
+
+export class XLSXParser {
+    public filePath: string;
+
+    public constructor(filePath: string) { 
+        this.filePath = filePath; 
+    }
+
+    public Execute() {
+        let workbook = xlsx.readFile(this.filePath);
+
+        console.log(workbook.Workbook.Names);
+    }
+}
